a64b09ae67a9a2c73228c7a300400838b8901313
Your Name <you@example.com>
2022-03-26 17:20:09 -0300

    Req 2 concluido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Informações sobre a Pokedex', () => {
+  it('Verifica se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+    const title = screen.getByRole('heading',
+      { name: /about pokédex/i, level: 2 });
+    expect(title).toBeInTheDocument();
+  });
+  it('Verifica se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    render(<About />);
+    const paragraphOne = screen.getByText(/This application simulates a Pokédex, a di/i);
+    const paragraphTwo = screen.getByText(/One can filter Pokémons by type, and see mo/i);
+    expect(paragraphOne).toBeInTheDocument();
+    expect(paragraphTwo).toBeInTheDocument();
+  });
+  it('Verifica se a página contém uma imagem específica', () => {
+    render(<About />);
+    const imgPokedex = screen.getByRole('img',
+      { name: /pokédex/i });
+    expect(imgPokedex.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
