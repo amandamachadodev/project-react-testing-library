0290592a0780266677da81c4c2957333c8246f2c
Your Name <you@example.com>
2022-03-26 18:40:11 -0300

    Req 3 concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Renderizando pokemons favoritos', () => {
+  it('Verifica se exibe uma mensagem quando não tiver pokémons favoritos', () => {
+    render(<FavoritePokemons />);
+    const mesage = screen.getByText(/no favorite pokemon found/i);
+    expect(mesage).toBeInTheDocument();
+  });
+  it('Verifica se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
+    expect(linkMoreDetails).toBeInTheDocument();
+
+    userEvent.click(linkMoreDetails);
+
+    const favorite = screen.getByText('Pokémon favoritado?');
+    expect(favorite).toBeInTheDocument();
+
+    userEvent.click(favorite);
+
+    expect(screen.getByLabelText('Pokémon favoritado?')).toBeChecked();
+
+    const favoritePokemons = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(favoritePokemons).toBeInTheDocument();
+
+    userEvent.click(favoritePokemons);
+
+    const pokemonFavorite = screen.getByText(/pikachu/i);
+    expect(pokemonFavorite).toBeInTheDocument();
+  });
+});
--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -4,10 +4,10 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 
 const renderWithRouter = (component) => {
-    const history = createMemoryHistory();
-    return ({
-      ...render(<Router history={history}>{component}</Router>), history,
-    });
-  };
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
+};
 
 export default renderWithRouter;
